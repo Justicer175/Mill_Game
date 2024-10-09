fc406fe01deed5c3dbf78b0b56d5e513ef65f092
Justicer175 <maksrezek@gmail.com>
2024-10-09 13:08:30 +0200

    Scripts for menu
    Add pop up on wrong selection (still needs polish),
    Start on creating game from given amount of cells and pieces

---
+++ Assets/Scripts/UI/PopUpScript.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PopUpScript : MonoBehaviour
+{
+    [Header("Internal")]
+    [SerializeField] private TMP_Text textField;
+    [HideInInspector] public StatePopUp state;
+    private RectTransform transformOfPopup;
+
+    [Header("Animation going up")]
+    [SerializeField] private int startingYofAnimation;
+    [SerializeField] private int endingYofAnimation;
+    [SerializeField] private int pixelsToMove;
+    [SerializeField] private int frameToMoveOn;
+    private int currentFrame = 0;
+
+    [Space (10.0f)]
+    [Header("Errors")]
+    [SerializeField] private string sameName;
+    [SerializeField] private string sameColor;
+    [SerializeField] private string toManyPieces;
+
+    //public static final Integer field = 200;
+
+    public enum Errors
+    {
+        SameName,
+        SameColor,
+        ToManyPieces
+    }
+
+    public enum StatePopUp
+    {
+        GoUp,
+        Wait,
+        GoDown
+    }
+
+    private void Update()
+    {
+        //create animation going up
+        switch (state)
+        {
+            case StatePopUp.GoUp:
+                PopupGoUp();
+                break;
+            case StatePopUp.Wait:
+                break;
+            case StatePopUp.GoDown:
+                PopupGoDown();
+                break;
+            default:
+                Debug.Log("Unknown state");
+                break;
+        }
+    }
+
+    public void ActivatePopup(Errors error)
+    {
+        gameObject.SetActive(true);
+        transformOfPopup = gameObject.GetComponent<RectTransform>();
+        state = StatePopUp.GoUp;
+        //Debug.Log(gameObject.transform.position.x + " "+ gameObject.transform.position.y);
+        gameObject.transform.position = new Vector2(gameObject.transform.position.x, startingYofAnimation);
+
+        switch (error)
+        { 
+            case Errors.SameName:
+                textField.text = sameName;
+                break;
+            case Errors.SameColor:
+                textField.text = sameColor;
+                break;
+            case Errors.ToManyPieces:
+                textField.text = toManyPieces;
+                break;
+            default:
+                textField.text = "Not defined error, Please define it in code";
+                break;
+        }
+
+
+    }
+
+    public void LeavePopUp()
+    {
+        state = StatePopUp.GoDown;
+    }
+
+    private void PopupGoUp()
+    {
+        if(currentFrame >= frameToMoveOn)
+        {
+            currentFrame = 0;
+            gameObject.transform.position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y + pixelsToMove);
+        }
+        else
+        {
+            currentFrame++;
+        }
+
+
+        if(transformOfPopup.offsetMax.y >= endingYofAnimation)
+        {
+            state = StatePopUp.Wait;
+        }
+    }
+
+    private void PopupGoDown()
+    {
+        gameObject.SetActive(false);
+    }
+
+}
--- Assets/Scripts/UI/SettingMenuScript.cs
+++ Assets/Scripts/UI/SettingMenuScript.cs
@@ -5,12 +5,18 @@ using TMPro;
 
 public class SettingMenuScript : MonoBehaviour
 {
+    [Header("Menu")]
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject settingsMenu;
+    [SerializeField] private PopUpScript popup;
+
     [Header("Player 1")]
     [SerializeField] private TMP_InputField p1Name;
-    //[SerializeField] private 
+    [SerializeField] private TMP_Dropdown p1Color;
 
     [Header("Player 2")]
     [SerializeField] private TMP_InputField p2Name;
+    [SerializeField] private TMP_Dropdown p2Color;
 
     // Start is called before the first frame update
     void Start()
@@ -22,5 +28,34 @@ public class SettingMenuScript : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void ReturnToMenu()
+    {
+        popup.ActivatePopup(PopUpScript.Errors.SameColor);
+        //if(CheckForName() && CheckForColor() && CheckForPieces())
+        //{
+        //    settingsMenu.SetActive(false);
+        //    mainMenu.SetActive(true);
+        //}
+        
+    }
+
+    private void CheckForName()
+    {
+
+    }
+
+    private void CheckForColor()
+    {
+        if(p1Color.value == p2Color.value)
+        {
+
+        }
+    }
+
+    private void CheckForPieces()
+    {
+
     }
 }
